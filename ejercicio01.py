9f93df28dd916b1a1474a9e946c3e49f8976b8b3
ruben-ruesga <rubensito3450@gmail.com>
2025-05-13 07:26:39 -0600

    Ejercicio 1

---
+++ ejercicio01.py
@@ -0,0 +1,6 @@
+################################################################################
+#  Escribir un programa que pregunte al usuario su nombre, y luego lo salude.
+################################################################################
+
+name = input ("Escribe tu nombre:  \n")
+print("hola " + name)
